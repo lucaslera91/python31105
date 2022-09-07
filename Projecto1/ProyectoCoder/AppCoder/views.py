d7293d5311cecede22fb359ebebda16889932a2a
lucaslera91 <lucaslera5@gmail.com>
2022-09-06 22:58:43 -0300

    bootstrap y herencia

--- Projecto1/ProyectoCoder/AppCoder/views.py
+++ Projecto1/ProyectoCoder/AppCoder/views.py
@@ -9,38 +9,41 @@ from AppCoder.models import Familiares
 
 
 
-def familiares(self):
+# def familiares(self):
 
-    miHtml = open('/Users/lucaslera/Documents/python-coder/djangoProjecto1/Projecto1/ProyectoCoder/AppCoder/plantillas/template1.html')
+#     miHtml = open('/Users/lucaslera/Documents/python-coder/djangoProjecto1/Projecto1/ProyectoCoder/AppCoder/plantillas/template1.html')
+#     plantilla = loader.get_template('template1.html')
+#     miHtml.close()
+#     mama = Familiares(nombre= "Mama" , fechaNacimiento= datetime.datetime(1978,11,11,), edad = 52 )
+#     mama.save()
+#     papa = Familiares(nombre= "Papa" , fechaNacimiento= datetime.datetime(1974,12,2), edad = 48 )
+#     papa.save()
+#     tio = Familiares(nombre= "Tio" , fechaNacimiento= datetime.datetime(1977,9,3), edad = 45 )
+#     tio.save()
+#     dateAux ={
+#         "mamaNacimiento": mama.fechaNacimiento.strftime("%x"),
+#         "papaNacimiento": papa.fechaNacimiento.strftime("%x"),
+#         "tioNacimiento": tio.fechaNacimiento.strftime("%x"),
+#     }
+#     familiaDic = {
+#         "mama": f"{mama.nombre} - Nacio: {dateAux.get('mamaNacimiento')} tiene {mama.edad} años!",
+#         "papa": f"{papa.nombre} - Nacio: {dateAux.get('papaNacimiento')} tiene {papa.edad} años!",
+#         "tio": f"{tio.nombre} - Nacio: {dateAux.get('tioNacimiento')} tiene {tio.edad} años!",
+#     }
+#     documento = plantilla.render(familiaDic)
+#     return HttpResponse(documento)
 
-    # plantilla = Template(miHtml.read())
-    plantilla = loader.get_template('template1.html')
-    miHtml.close()
+def inicio(request):
+    return render(request, "AppCoder/inicio.html")
 
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
 
-    mama = Familiares(nombre= "Mama" , fechaNacimiento= datetime.datetime(1978,11,11,), edad = 52 )
-    mama.save()
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
 
-    papa = Familiares(nombre= "Papa" , fechaNacimiento= datetime.datetime(1974,12,2), edad = 48 )
-    papa.save()
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
 
-    tio = Familiares(nombre= "Tio" , fechaNacimiento= datetime.datetime(1977,9,3), edad = 45 )
-    tio.save()
-
-    dateAux ={
-        "mamaNacimiento": mama.fechaNacimiento.strftime("%x"),
-        "papaNacimiento": papa.fechaNacimiento.strftime("%x"),
-        "tioNacimiento": tio.fechaNacimiento.strftime("%x"),
-    }
-    familiaDic = {
-        "mama": f"{mama.nombre} - Nacio: {dateAux.get('mamaNacimiento')} tiene {mama.edad} años!",
-        "papa": f"{papa.nombre} - Nacio: {dateAux.get('papaNacimiento')} tiene {papa.edad} años!",
-        "tio": f"{tio.nombre} - Nacio: {dateAux.get('tioNacimiento')} tiene {tio.edad} años!",
-    }
-    
-    documentoDeTexto = f"<h2>-->{mama.nombre} - Nacio: {mama.edad} tiene {mama.edad} años<h2><br><h2>-->{papa.nombre} - Nacio: {papa.edad} tiene {mama.edad} años<h2><br><h2>-->{mama.nombre} - Nacio: {tio.edad} tiene {tio.edad} años<h2>"
-    miContexto = Context(familiaDic)
-    documento = plantilla.render(miContexto)
-  
-    return HttpResponse(documento)
-    # return HttpResponse(documentoDeTexto)
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
--- Projecto1/ProyectoCoder/ProyectoCoder/settings.py
+++ Projecto1/ProyectoCoder/ProyectoCoder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ProyectoCoder.urls'
-
+# 'DIRS': [os.path.join(BASE_DIR, 'AppCoder')],
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/lucaslera/Desktop/testPython/python31105/Projecto1/ProyectoCoder/AppCoder/plantillas'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
