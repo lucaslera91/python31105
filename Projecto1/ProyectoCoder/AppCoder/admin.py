5cef08d0c952c293851d85df73d2ffdf0604873a
lucaslera91 <lucaslera5@gmail.com>
2022-09-06 23:50:13 -0300

    data updated

--- Projecto1/ProyectoCoder/AppCoder/admin.py
+++ Projecto1/ProyectoCoder/AppCoder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Estudiantes)
+admin.site.register(Profesor)
+admin.site.register(Entregable)
